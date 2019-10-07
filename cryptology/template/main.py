578839a0f1885c5ef38abc195f344575123a3175
linjunhan <m15521093428@163.com>
2019-10-07 17:17:29 +0800

    lastandlast

--- cryptology/template/main.py
+++ cryptology/template/main.py
@@ -26,7 +26,7 @@ save_button.Bind(wx.EVT_BUTTON, ciptolotion)
 
 content_text = wx.TextCtrl(frame, pos=(5, 39), size=(475, 300), style=wx.TE_MULTILINE)
 content_text.write("\n\n\n\n使用说明：将你需要加密的文件路径填入上方的路径框中，点击加密即可加密文件，可以多次加密，点击解密即可解密文件。\n"
-                   "\n\n注意：该程序目前只能加密txt文件。\n\n 该加密程序的密钥文件存在视图层的静态文件中。")
+                   "\n\n注意：该程序目前只能加密 txt 文件。\n\n 该加密程序的密钥文件存在视图层的静态文件中。")
 #  wx.TE_MULTILINE可以实现以滚动条方式多行显示文本,若不加此功能文本文档显示为一行
 
 frame.Show()
